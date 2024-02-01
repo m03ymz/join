66e0ea2855d8e3d5f1c44b17507dbef72a22b675
Muhammed Yilmaz <moe03@live.de>
2024-02-01 15:21:21 +0100

    open & close task overlay

--- js/board.js
+++ js/board.js
@@ -1,3 +1,14 @@
 function openOverlay(){
-    
+    document.getElementById(`task-overlay`).style.right = "500px";
+    document.getElementById(`content-board`).classList.add('bg_darker_board');
+    // document.getElementById(`w3-include-board`).classList.add('bg_darker_board');
+    document.getElementById(`content-board`).classList.add('pointer_events-none');
+    document.getElementById(`body`).classList.add('overflow_hidden');
+}
+
+function closeOverlay(){
+    document.getElementById(`task-overlay`).style.right = "-700px";
+    document.getElementById(`content-board`).classList.remove('bg_darker_board');
+    document.getElementById(`content-board`).classList.remove('pointer_events-none');
+    document.getElementById(`body`).classList.remove('overflow_hidden');
 }
