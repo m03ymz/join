7f94481197acb140ec2e69602f91e7af03cda7f9
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-02-01 11:40:30 +0100

    update outsourced

---
+++ js/contacts.js
@@ -0,0 +1,16 @@
+function hidePopup() {
+    document.getElementById('popupBackground').style.right = '-100%';
+}
+
+   function showPopup() {
+   document.getElementById('popupBackground').style.right = '0';
+}
+
+   function togglePopup() {
+   let popup = document.getElementById('popupBackground');
+   if (popup.style.right === '0') {
+       hidePopup();
+   } else {
+       showPopup();
+   }
+}
