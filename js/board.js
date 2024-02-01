daee11b62f589b6590c677e5dcebeb745caa222e
Muhammed Yilmaz <moe03@live.de>
2024-02-01 19:55:38 +0100

    task overlay update

--- js/board.js
+++ js/board.js
@@ -1,14 +1,13 @@
 function openOverlay(){
     document.getElementById(`task-overlay`).style.right = "500px";
-    document.getElementById(`content-board`).classList.add('bg_darker_board');
-    // document.getElementById(`w3-include-board`).classList.add('bg_darker_board');
+    document.getElementById(`overlay`).style.display = "block";
     document.getElementById(`content-board`).classList.add('pointer_events-none');
     document.getElementById(`body`).classList.add('overflow_hidden');
 }
 
 function closeOverlay(){
     document.getElementById(`task-overlay`).style.right = "-700px";
-    document.getElementById(`content-board`).classList.remove('bg_darker_board');
+    document.getElementById(`overlay`).style.display = "none";
     document.getElementById(`content-board`).classList.remove('pointer_events-none');
     document.getElementById(`body`).classList.remove('overflow_hidden');
 }
