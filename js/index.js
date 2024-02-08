4152e5f4eb8db53960f946089278a9b16ffc4fbe
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-02-08 20:08:06 +0100

    Animation final href login

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ function initIndex() {
 }
 window.onload = function() {
     setTimeout(function() {
-        window.location.href = "summary.html";
+        window.location.href = "log_in.html";
     }, 1000); // Verzögerung von 5000 Millisekunden (5 Sekunden)
 }
