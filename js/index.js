5b07fa13751d06f2b38687e9782cdb351395fa16
SoufianElMakhfi <soufian.elmakhfi@gmail.com>
2024-02-08 20:05:32 +0100

    index animation finish

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 function initIndex() {
 }
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "summary.html";
+    }, 1000); // Verzögerung von 5000 Millisekunden (5 Sekunden)
+}
